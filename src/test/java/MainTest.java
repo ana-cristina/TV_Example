a40fbb3325d2bc24b73dbc5e4cb15102cc49e976
anaturlea <turleaana@gmail.com>
2023-02-23 10:42:47 +0200

    created project and added main files

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,63 @@
+public class MainTest {
+//
+//    @Test
+//    void verificaTrue(){
+//        Assertions.assertTrue(Main.calculeaza(6533).equals("DA"));
+//    }
+//    @Test
+//    void verificaFalse(){
+//        Assertions.assertTrue(Main.calculeaza(6282).equals("NU"));
+//    }
+//
+//    @Test
+//    void testarePartitionareInClaseDeEchivalenta(){
+//
+//
+//        Assertions.assertTrue(Main.calculeaza(4513).equals("DA"));
+//        Assertions.assertTrue(Main.calculeaza(1234).equals("NU"));
+//        Assertions.assertTrue(Main.calculeaza(345).equals("Numarul nu este valid"));
+//        Assertions.assertTrue(Main.calculeaza(123456).equals("Numarul nu este valid"));
+//
+//    }
+//    @Test
+//    void frontiere(){
+///*
+//        (1000, “NU”)
+//        (99999, “NU”)
+//        (4513, “DA”)
+//        (999, “eroare”)
+//        (34, “eroare”)
+//        (100000, “eroare”)
+//        (4534123, “eroare”)
+//        */
+//
+//        Assertions.assertTrue(Main.calculeaza(1000).equals("NU"));
+//        Assertions.assertTrue(Main.calculeaza(99999).equals("NU"));
+//        Assertions.assertTrue(Main.calculeaza(4513).equals("DA"));
+//        Assertions.assertTrue(Main.calculeaza(999).equals("Numarul nu este valid"));
+//        Assertions.assertTrue(Main.calculeaza(34).equals("Numarul nu este valid"));
+//        Assertions.assertTrue(Main.calculeaza(100000).equals("Numarul nu este valid"));
+//        Assertions.assertTrue(Main.calculeaza(4534123).equals("Numarul nu este valid"));
+//    }
+//
+//    @Test
+//    void grafulcauzaefect(){
+//
+//        /*
+//        n
+//123
+//1231
+//1234
+//output
+//eroare
+//DA
+//NU
+//
+//
+//         */
+//
+//        Assertions.assertTrue(Main.calculeaza(123).equals("Numarul nu este valid"));
+//        Assertions.assertTrue(Main.calculeaza((1231)).equals("DA"));
+//        Assertions.assertTrue(Main.calculeaza((1234)).equals("NU"));
+//    }
+}
